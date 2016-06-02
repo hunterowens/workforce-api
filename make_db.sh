b88131944885793c66e0ca4f5433db2f097db936
hunterowens <owens.hunter@gmail.com>
2016-06-02 09:50:32 -0500

    if stmts

--- make_db.sh
+++ make_db.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 set -e
+if [ ! -f ./skills_vector_distance.tsv ]; then
+  aws s3 cp s3://skills-public/bucket/top_n/skills_vector_distance.tsv .
+fi
+csvsql -t --insert  --db postgres://hunterowens:@localhost/labor skills_vector_distance.tsv
 
-csvsql -t --insert  --db postgres://hunterowens:@localhost/labor corpus_counts.tsv
+if [ ! -f ./skills_master_table.tsv ]; then
+  aws s3 cp s3://skills-public/bucket/onet/skills_master_table.tsv .
+fi
+csvsql -t --insert  --db postgres://hunterowens:@localhost/labor skills_master_table.tsv
