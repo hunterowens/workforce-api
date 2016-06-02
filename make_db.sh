864caf0fb28f580a9155d606825b4c617612bc4a
hunterowens <owens.hunter@gmail.com>
2016-06-02 12:28:43 -0500

    check check check

--- make_db.sh
+++ make_db.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -e
-if [ ! -f ./skills_vector_distance.tsv ]; then
+if [ ! -f ./skills_master_table.tsv ]; then
   aws s3 cp s3://skills-public/bucket/top_n/skills_vector_distance.tsv .
 fi
+
 csvsql -t --insert  --db postgres://hunterowens:@localhost/labor skills_vector_distance.tsv
 
 if [ ! -f ./skills_master_table.tsv ]; then
